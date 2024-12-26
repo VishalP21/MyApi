540b5e138c61cf499d58d81765f56629f4ce7efe
Vishal Prajapati <vishalp951295@gmail.com>
2024-12-26 20:07:02 +0530

    Add project files.

---
+++ MyApi/Controllers/WeltecController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeltecController : ControllerBase
+    {
+
+        WeltecdatabaseContext weltecdatabaseContext;
+
+        public WeltecController()
+        {
+            weltecdatabaseContext = new WeltecdatabaseContext();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var data = weltecdatabaseContext.CourseWeltacs.ToList();
+            return Ok(data);
+        }
+
+        [HttpPost]
+
+        public IActionResult Post(CourseWeltac course)
+        {
+            weltecdatabaseContext.CourseWeltacs.Add(course);
+            weltecdatabaseContext.SaveChanges();
+            return Ok();
+        }
+
+
+        [HttpDelete]
+
+        public IActionResult Delete(int id)
+        {
+            var course = weltecdatabaseContext.CourseWeltacs.Find(id);
+            weltecdatabaseContext.CourseWeltacs.Remove(course);
+            weltecdatabaseContext.SaveChanges();
+            return Ok();
+        }
+
+        //[HttpPut]
+
+        //public IActionResult Put(CourseWeltac course)
+        //{
+        //    weltecdatabaseContext.CourseWeltacs.Update(course);
+        //    weltecdatabaseContext.SaveChanges();
+        //    return Ok();
+        //}
+
+        [HttpPut]
+
+        public IActionResult update(CourseWeltac course)
+        {
+           var courses = weltecdatabaseContext.CourseWeltacs.Where(x => x.Id == course.Id).FirstOrDefault();
+            courses.Id = course.Id;
+            courses.Name = course.Name;
+
+            weltecdatabaseContext.SaveChanges();
+            return Ok();
+        }
+    }
+}
